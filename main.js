7f0ac471e7e6aaffa7db1525be557fcf7021f2aa
natia <natia.karamnishvili369@law.tsu.ge>
2025-03-23 00:53:47 +0400

    foiurth

--- main.js
+++ main.js
@@ -32,73 +32,7 @@ const products = [
     name: "Macaron Mix of Five",
     category: "Macaron",
     price: 8.0,
-  },
-  {
-    image: {
-      thumbnail: "./assets/images/image-tiramisu-thumbnail.jpg",
-      mobile: "./assets/images/image-tiramisu-mobile.jpg",
-      tablet: "./assets/images/image-tiramisu-tablet.jpg",
-      desktop: "./assets/images/image-tiramisu-desktop.jpg",
-    },
-    name: "Classic Tiramisu",
-    category: "Tiramisu",
-    price: 5.5,
-  },
-  {
-    image: {
-      thumbnail: "./assets/images/image-baklava-thumbnail.jpg",
-      mobile: "./assets/images/image-baklava-mobile.jpg",
-      tablet: "./assets/images/image-baklava-tablet.jpg",
-      desktop: "./assets/images/image-baklava-desktop.jpg",
-    },
-    name: "Pistachio Baklava",
-    category: "Baklava",
-    price: 4.0,
-  },
-  {
-    image: {
-      thumbnail: "./assets/images/image-meringue-thumbnail.jpg",
-      mobile: "./assets/images/image-meringue-mobile.jpg",
-      tablet: "./assets/images/image-meringue-tablet.jpg",
-      desktop: "./assets/images/image-meringue-desktop.jpg",
-    },
-    name: "Lemon Meringue Pie",
-    category: "Pie",
-    price: 5.0,
-  },
-  {
-    image: {
-      thumbnail: "./assets/images/image-cake-thumbnail.jpg",
-      mobile: "./assets/images/image-cake-mobile.jpg",
-      tablet: "./assets/images/image-cake-tablet.jpg",
-      desktop: "./assets/images/image-cake-desktop.jpg",
-    },
-    name: "Red Velvet Cake",
-    category: "Cake",
-    price: 4.5,
-  },
-  {
-    image: {
-      thumbnail: "./assets/images/image-brownie-thumbnail.jpg",
-      mobile: "./assets/images/image-brownie-mobile.jpg",
-      tablet: "./assets/images/image-brownie-tablet.jpg",
-      desktop: "./assets/images/image-brownie-desktop.jpg",
-    },
-    name: "Salted Caramel Brownie",
-    category: "Brownie",
-    price: 4.5,
-  },
-  {
-    image: {
-      thumbnail: "./assets/images/image-panna-cotta-thumbnail.jpg",
-      mobile: "./assets/images/image-panna-cotta-mobile.jpg",
-      tablet: "./assets/images/image-panna-cotta-tablet.jpg",
-      desktop: "./assets/images/image-panna-cotta-desktop.jpg",
-    },
-    name: "Vanilla Panna Cotta",
-    category: "Panna Cotta",
-    price: 6.5,
-  },
+  }
 ];
 const order = [];
 
